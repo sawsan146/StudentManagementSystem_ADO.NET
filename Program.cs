db010a715044b72cce70e025bd21c3ec0c9b5ab2
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2025-03-02 22:16:44 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentManagementSystem_ADO.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
